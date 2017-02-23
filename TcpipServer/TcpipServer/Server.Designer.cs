50d79f8fd5970b6c4e6456809974986d7a0e14d6
mac <acidapple2010@mail.ru>
2017-02-23 17:38:31 +0300

    adding server

---
+++ TcpipServer/TcpipServer/Server.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace TcpipServer
+{
+    partial class Server
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBoxLst = new System.Windows.Forms.GroupBox();
+            this.tbConsoleOut = new System.Windows.Forms.RichTextBox();
+            this.btnStartListening = new System.Windows.Forms.Button();
+            this.tbPort = new System.Windows.Forms.TextBox();
+            this.tbIPAddress = new System.Windows.Forms.TextBox();
+            this.btnClean = new System.Windows.Forms.Button();
+            this.connectionGroup = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBoxLst.SuspendLayout();
+            this.connectionGroup.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBoxLst
+            // 
+            this.groupBoxLst.Controls.Add(this.btnClean);
+            this.groupBoxLst.Controls.Add(this.tbConsoleOut);
+            this.groupBoxLst.Location = new System.Drawing.Point(12, 106);
+            this.groupBoxLst.Name = "groupBoxLst";
+            this.groupBoxLst.Size = new System.Drawing.Size(302, 286);
+            this.groupBoxLst.TabIndex = 0;
+            this.groupBoxLst.TabStop = false;
+            this.groupBoxLst.Text = "Listen";
+            // 
+            // tbConsoleOut
+            // 
+            this.tbConsoleOut.Location = new System.Drawing.Point(6, 13);
+            this.tbConsoleOut.Name = "tbConsoleOut";
+            this.tbConsoleOut.Size = new System.Drawing.Size(290, 231);
+            this.tbConsoleOut.TabIndex = 1;
+            this.tbConsoleOut.Text = "";
+            // 
+            // btnStartListening
+            // 
+            this.btnStartListening.Location = new System.Drawing.Point(197, 41);
+            this.btnStartListening.Name = "btnStartListening";
+            this.btnStartListening.Size = new System.Drawing.Size(99, 32);
+            this.btnStartListening.TabIndex = 2;
+            this.btnStartListening.Text = "Listen";
+            this.btnStartListening.UseVisualStyleBackColor = true;
+            this.btnStartListening.Click += new System.EventHandler(this.btnStartListening_Click);
+            // 
+            // tbPort
+            // 
+            this.tbPort.Location = new System.Drawing.Point(67, 48);
+            this.tbPort.Name = "tbPort";
+            this.tbPort.Size = new System.Drawing.Size(124, 20);
+            this.tbPort.TabIndex = 1;
+            // 
+            // tbIPAddress
+            // 
+            this.tbIPAddress.Location = new System.Drawing.Point(67, 13);
+            this.tbIPAddress.Name = "tbIPAddress";
+            this.tbIPAddress.Size = new System.Drawing.Size(229, 20);
+            this.tbIPAddress.TabIndex = 0;
+            // 
+            // btnClean
+            // 
+            this.btnClean.Location = new System.Drawing.Point(197, 250);
+            this.btnClean.Name = "btnClean";
+            this.btnClean.Size = new System.Drawing.Size(99, 30);
+            this.btnClean.TabIndex = 2;
+            this.btnClean.Text = "Clean";
+            this.btnClean.UseVisualStyleBackColor = true;
+            this.btnClean.Click += new System.EventHandler(this.btnClean_Click);
+            // 
+            // connectionGroup
+            // 
+            this.connectionGroup.Controls.Add(this.btnStartListening);
+            this.connectionGroup.Controls.Add(this.tbPort);
+            this.connectionGroup.Controls.Add(this.tbIPAddress);
+            this.connectionGroup.Controls.Add(this.label2);
+            this.connectionGroup.Controls.Add(this.label4);
+            this.connectionGroup.Location = new System.Drawing.Point(12, 13);
+            this.connectionGroup.Name = "connectionGroup";
+            this.connectionGroup.Size = new System.Drawing.Size(302, 87);
+            this.connectionGroup.TabIndex = 11;
+            this.connectionGroup.TabStop = false;
+            this.connectionGroup.Text = "Connection";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Server IP: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(4, 51);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Server Port: ";
+            // 
+            // Server
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(324, 404);
+            this.Controls.Add(this.connectionGroup);
+            this.Controls.Add(this.groupBoxLst);
+            this.Name = "Server";
+            this.Text = "Server";
+            this.Load += new System.EventHandler(this.Server_Load);
+            this.groupBoxLst.ResumeLayout(false);
+            this.connectionGroup.ResumeLayout(false);
+            this.connectionGroup.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxLst;
+        private System.Windows.Forms.Button btnStartListening;
+        private System.Windows.Forms.TextBox tbPort;
+        private System.Windows.Forms.TextBox tbIPAddress;
+        private System.Windows.Forms.Button btnClean;
+        private System.Windows.Forms.GroupBox connectionGroup;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox tbConsoleOut;
+    }
+}
+
---
+++ TcpipServer/TcpipServer/Server.cs
@@ -0,0 +1,331 @@
+﻿using System;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+
+//Imports for Sockets Programming
+using System.Net;
+using System.Net.Sockets;
+using Mono.Data.Sqlite;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace TcpipServer
+{
+
+	public partial class Server : Form
+	{
+		
+		TcpListener mTcpListener;
+		TcpClient mTcpClient;
+		byte[] mRx;
+		SqliteConnection connpar;
+		DataSet dspar;
+		string svChangeFlag, svChangeNum;
+		string filename_dbinp = @"../../../inpar_Kulygin_1.sqlite";
+		string filename_db_server = @"../../../db_for_server.sqlite";
+
+		public Server()
+		{
+			InitializeComponent();
+			tbConsoleOut.TabStop = false;
+		}
+
+		//Находим IP адрес компьютера
+		IPAddress FindMyIpv4Address()
+		{
+			string strThisHostName = string.Empty;
+			IPHostEntry thisHostDNSEntry = null;
+			IPAddress[] allIPsOfThisHost = null;
+			IPAddress ipv4Ret = null;
+
+			try
+			{
+				//получаем имя хоста системы и храним его в строке
+				strThisHostName = Dns.GetHostName();
+				PrintLine("Host Name: " + strThisHostName);
+				//делаем хост именем домена сервера и храним его в IPHostEntry
+				thisHostDNSEntry = Dns.GetHostEntry(strThisHostName);
+				//получаем все IP-адрес и сохраним их в хост-объекта
+				allIPsOfThisHost = thisHostDNSEntry.AddressList;
+
+				//перебераем все адреса и оставляем первый IPv4
+				for (int idx = allIPsOfThisHost.Length - 1; idx >= 0; idx--)
+				{
+					if (allIPsOfThisHost[idx].AddressFamily == AddressFamily.InterNetwork)
+					{
+						ipv4Ret = allIPsOfThisHost[idx];
+						break;
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message, "TCP/IP Server", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+			return ipv4Ret;
+		}
+
+		private void Server_Load(object sender, EventArgs e)
+		{
+			IPAddress Ipa = null;
+			Ipa = FindMyIpv4Address();
+
+			if (Ipa != null)
+			{
+				tbIPAddress.Text = Ipa.ToString();
+			}
+		}
+
+		private void btnStartListening_Click(object sender, EventArgs e)
+		{
+			IPAddress ipaddr;
+			int nPort;
+
+			//проверяем, если IP - адрес и номер порта действителен
+			if (!int.TryParse(tbPort.Text, out nPort))
+			{
+				nPort = 9090;
+				tbPort.Text = Convert.ToString(nPort);
+			}
+
+			if (!IPAddress.TryParse(tbIPAddress.Text, out ipaddr))
+			{
+				MessageBox.Show("Invalid IPv4 address supplied.", "TCP/IP Server", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+
+			//Мы дважды проверьте, если мы еще способны подключаться к tcplistener с
+			try
+			{
+				//если все пойдет хорошо, то мы создадим tcplistener с разбора IP-адрес и порт
+				mTcpListener = new TcpListener(ipaddr, nPort);
+
+				//начинаем подключение
+				mTcpListener.Start();
+
+				//начинаем принимать подключения от клиента
+				mTcpListener.BeginAcceptTcpClient(OnCompleteAcceptTcpClient, mTcpListener);
+
+				PrintLine("Server is listening and waiting for client to connect...");
+
+			}
+			catch (Exception ex)
+			{
+				PrintLine(ex.Message);
+				Application.Restart();
+			}
+
+		}
+
+		//написать и отправить поток клиенту
+		private void onCompleteWriteToClientStream(IAsyncResult iar)
+		{
+			//Вот все, что нам нужно сделать, это закончить операцию, когда пакеты посылаются
+			try
+			{
+				TcpClient tcpc = (TcpClient)iar.AsyncState;
+				tcpc.GetStream().EndWrite(iar);
+
+			}
+			catch (Exception ex)
+			{
+				PrintLine(ex.Message);
+				//MessageBox.Show(ex.Message, "TCP/IP Server", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+		}
+
+		//принять поток вызова
+		void OnCompleteAcceptTcpClient(IAsyncResult iar)
+		{
+			//преобразовываем статус иар для tcplistener
+			TcpListener tcpl = (TcpListener)iar.AsyncState;
+			try
+			{
+				//ловим каждый AsyncCall так, что соединение принимается непрерывно
+				mTcpClient = tcpl.EndAcceptTcpClient(iar);
+
+				OpenData();
+				GetIndex();
+				var tx = Encoding.Unicode.GetBytes(svChangeFlag);
+				mTcpClient.GetStream().BeginWrite(tx, 0, tx.Length, onCompleteWriteToClientStream, mTcpClient);
+
+				//в то время как один клиент подключен, мы можем позволить другим клиентам подключить
+				tcpl.BeginAcceptTcpClient(OnCompleteAcceptTcpClient, tcpl);
+
+				//установливаем количество разрешенных по умолчанию байт
+				mRx = new byte[2];
+				mTcpClient.GetStream().BeginRead(mRx, 0, mRx.Length, OnCompleteReadFromTcpClientStream, mTcpClient);
+
+			}
+			catch (Exception ex)
+			{
+				PrintLine(ex.Message);
+			}
+		}
+
+		bool updateTable;
+		string strRecvSize;
+		//читаем поток данных
+		void OnCompleteReadFromTcpClientStream(IAsyncResult iar)
+		{
+			TcpClient tcpc;
+			int nCountReadBytes = 0;
+			string strRecv;
+
+			try
+			{
+				//синхронизируем результат в объект tcpclient
+				tcpc = (TcpClient)iar.AsyncState;
+
+				//расчитываем количество байт в int
+				nCountReadBytes = tcpc.GetStream().EndRead(iar);
+
+				//проверяем, если есть еще пакеты
+				if (nCountReadBytes == 0)
+				{
+					PrintLine("Client has been disconnected. Idle for too long...");
+					return;
+				}
+
+				//обновляем полученные таблицы в базе 
+				//UpdateTable(filename_dbinp, ConvertByteArrayToDataTable(mRx));
+				strRecv = Encoding.Unicode.GetString(mRx, 0, nCountReadBytes);
+
+				//что-то придумать насчет большого количества передаваемых байтов в пакете, 
+				//очищаем буфер, поэтому клиенты могут непрерывно посылать пакеты
+				if (strRecv.Equals("-") || strRecv.Equals(" "))
+				{
+					if (!strRecvSize.Equals(null))
+					{
+						mRx = new byte[Convert.ToInt32(strRecvSize)];
+						strRecvSize = null;
+					}
+					else
+						mRx = new byte[254];
+					//- для понимания, что этот массив байт таблица, которую нужно обновить
+					updateTable |= strRecv.Equals("-");
+				}
+				else
+				{
+					if (nCountReadBytes == 2)
+						strRecvSize += strRecv;
+					else if (updateTable)
+					{
+						UpdateTable(filename_dbinp, ConvertByteArrayToDataTable(mRx));
+						updateTable = false;
+					}
+					mRx = new byte[2];
+				}
+				PrintLine(strRecv);
+				tcpc.GetStream().BeginRead(mRx, 0, mRx.Length, OnCompleteReadFromTcpClientStream, tcpc);
+
+			}
+			catch (Exception ex)
+			{
+				PrintLine(ex.Message);
+			}
+		}
+
+		public void PrintLine(string strPrint)
+		{
+			tbConsoleOut.Invoke(new Action<string>(DoInvoke), strPrint);
+		}
+
+		public void DoInvoke(string strPrint)
+		{
+			tbConsoleOut.Text = tbConsoleOut.Text + strPrint + Environment.NewLine;
+		}
+
+		private void btnClean_Click(object sender, EventArgs e)
+		{
+            tbConsoleOut.Clear();
+		}
+
+		#region открытие бд
+		private void OpenData()
+		{
+
+			try
+			{
+				connpar = new SqliteConnection("data source=" + filename_dbinp + ";version=3;failifmissing=true;");
+				connpar.Open();
+				dspar = DataSetParsLoader(connpar);
+				connpar.Close();
+			}
+			catch (Exception ex)
+			{
+				PrintLine(ex.Message);
+			}
+		}
+
+		private DataSet DataSetParsLoader(SqliteConnection connection)
+		{
+			var dataset = new DataSet();
+
+			string sqlcmd = "select * from LST_INPAR";
+			var da = new SqliteDataAdapter(sqlcmd, connection);
+			da.Fill(dataset, "LST_INPAR");
+
+			sqlcmd = "select * from LST_ITEM";
+			da = new SqliteDataAdapter(sqlcmd, connection);
+			da.Fill(dataset, "LST_ITEM");
+
+			sqlcmd = "select * from LST_CHANGE";
+			da = new SqliteDataAdapter(sqlcmd, connection);
+			da.Fill(dataset, "LST_CHANGE");
+
+			return dataset;
+		}
+		#endregion
+
+		public void GetIndex()
+		{
+			foreach (DataRow dr in dspar.Tables["LST_CHANGE"].Rows)
+			{
+				svChangeFlag = dr["CHANGE"].ToString();
+				svChangeNum = dr["CHANGENUM"].ToString();
+				PrintLine(svChangeFlag + " / " + svChangeNum);
+			}
+		}
+
+		private static DataTable ConvertByteArrayToDataTable(byte[] byteDataArray)
+		{
+			DataTable dataTable;
+			var brFormatter = new BinaryFormatter();
+			using (var memStream = new MemoryStream(byteDataArray))
+			{
+				dataTable = (DataTable)brFormatter.Deserialize(memStream);
+				memStream.Close();
+			}
+			return dataTable;
+		}
+
+		public static void UpdateTable(string filename_dbinp, DataTable dataTable)
+		{
+			using (var connect = new SqliteConnection("data source=" + filename_dbinp + ";version=3;failifmissing=true;"))
+			{
+				connect.Open();
+				using (var sqlCommand = connect.CreateCommand())
+				{
+					sqlCommand.CommandText = "delete from " + dataTable.TableName;
+					sqlCommand.ExecuteNonQuery();
+
+					string values;
+					foreach (DataRow item in dataTable.Rows)
+					{
+						values = null;
+						values += "'" + item.ItemArray[0] + "'";
+						for (int i = 1; i < item.ItemArray.Length; i++)
+						{
+							values += "," + "'" + item.ItemArray[i] + "'";
+						}
+						sqlCommand.CommandText = "INSERT INTO " + dataTable.TableName + " VALUES(" + values + ")";
+						sqlCommand.ExecuteNonQuery();
+					}
+				}
+				connect.Close();
+			}
+		}
+	}
+}
