b9798d275ad92c1c46327b477ca56bef3ca51a74
mac <acidapple2010@mail.ru>
2017-02-25 11:33:18 +0300

    update

--- TcpipServer/TcpipServer/ConnectionToDB.cs
+++ TcpipServer/TcpipServer/ConnectionToDB.cs
@@ -12,15 +12,12 @@ namespace TcpipServer
 			connection = new SqliteConnection("data source=" + filename + ";version=3;failifmissing=true;");
 		}
 
-		public DataSet DataSetDB(string name_table, DataSet dataset, string sqlcmd)
+		public DataSet DataSetSelect(string name_table, DataSet dataset, string sqlcmd)
 		{
 			connection.Open();
-			//var cmd = "select * from " + name_table;
 			var da = new SqliteDataAdapter(sqlcmd, connection);
 			connection.Close();
-
 			da.Fill(dataset, name_table);
-
 			return dataset;
 		}
 	}
