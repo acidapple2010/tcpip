f337f24dedf9434141ef1afbc6ca99d8b298efb5
mac <acidapple2010@mail.ru>
2017-03-04 16:03:30 +0300

    serv

--- TcpipServer/TcpipServer/ConnectionToDB.cs
+++ TcpipServer/TcpipServer/ConnectionToDB.cs
@@ -1,22 +1,22 @@
 ﻿using System.Data;
-//using Mono.Data.Sqlite;
-using System.Data.SQLite;
+using Mono.Data.Sqlite;
+//using System.Data.SQLite;
 
 namespace TcpipServer
 {
 	public class ConnectionToDB
 	{
-		readonly SQLiteConnection connection;
+		readonly SqliteConnection connection;
 
 		public ConnectionToDB(string filename)
 		{
-			connection = new SQLiteConnection("data source=" + filename + ";version=3;failifmissing=true;");
+			connection = new SqliteConnection("data source=" + filename + ";version=3;failifmissing=true;");
 		}
 
 		public DataSet DSsqlcmdToDB(string name_table, DataSet dataset, string sqlcmd)
 		{
 			connection.Open();
-			var da = new SQLiteDataAdapter(sqlcmd, connection);
+			var da = new SqliteDataAdapter(sqlcmd, connection);
 			connection.Close();
 			da.Fill(dataset, name_table);
 			return dataset;
@@ -25,7 +25,7 @@ namespace TcpipServer
 		public void DSsqlcmdToDB(string sqlcmd)
 		{
 			connection.Open();
-			var command = new SQLiteCommand(sqlcmd, connection);
+			var command = new SqliteCommand(sqlcmd, connection);
 			command.ExecuteNonQuery();
 			connection.Close();
 		}
